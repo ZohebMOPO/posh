c6adebd799bd5427f527643e212a95491e158be6
ZohebMOPO <understandingzak@gmail.com>
2022-10-11 15:14:02 +0530

    First

---
+++ src/ci/interpreters/posh/token.java
@@ -0,0 +1,19 @@
+package ci.interpreters.posh;
+
+public class token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
